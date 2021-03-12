85fad603820b4b21c3ff2f0ac524ee58e64c887f
baciiu <albaciu@edu.aau.at>
2021-03-12 18:47:37 +0200

    implemented the calculation for the button

--- app/src/main/java/com/example/einzelphase/MainActivity.java
+++ app/src/main/java/com/example/einzelphase/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void berechung(View view) {
+
+        // 11732093 mod 7 = 2 => replace very second number with ASCII character
+
+        TextView gib_matnr = findViewById(R.id.gib_matnr);
+        EditText matnr_input = findViewById(R.id.matnr_input);
+
+        int matnr = Integer.parseInt(matnr_input.getText().toString());
+
+        String solution = "" + matnr;
+        String ascii = "ABCDEFGHIO" ;
+
+        for(int i = 0  ; i < solution.length()-1 ; i+= 2){
+
+            solution.replace(solution.charAt(i),ascii.charAt(i));
+        }
+
+        gib_matnr.setText(solution);
+        // server
+    }
+
+}
 
 
